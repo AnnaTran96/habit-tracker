8e8862ba2b1fb14e367c494adcd418ae47e4d503
Anna Tran <annatran@annas-air.mynet>
2020-09-04 16:09:50 +0100

    add database

---
+++ Server/database/config.js
@@ -0,0 +1,9 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({ database: 'tracker'});
+
+function run(query, values, callback) {
+    return pool.query(query, values, callback)
+}
+
+module.exports = { run };
