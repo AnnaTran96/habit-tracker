20c983ede1940d7526569d125fcb56fd1fdcbe2e
Anna Tran <annatran@annas-air.mynet>
2020-09-08 17:37:44 +0100

    add frequency

--- Server/database/queries.js
+++ Server/database/queries.js
@@ -1,6 +1,6 @@
-const index = `SELECT * FROM habits`;
-const show = `SELECT * FROM habits WHERE id = $1`;
-const create = `INSERT INTO habits (name, count) VALUES ($1, $2) RETURNING *`;
+const index = `SELECT * FROM habits ORDER BY id`;
+const show = `SELECT * FROM habits WHERE id = $1 ORDER BY id`;
+const create = `INSERT INTO habits (name, count, frequency) VALUES ($1, $2, $3) RETURNING *`;
 const update = `UPDATE habits SET count = count + 1 WHERE id = $1 RETURNING *`;
 
 module.exports = { index, show, create, update };
