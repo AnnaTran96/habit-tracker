4c09a263e8645e5aa8345bc7ffffc6f7933d02c9
Anna Tran <annatran@annas-air.mynet>
2020-09-08 09:30:40 +0100

    add routes

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import { Login, AddNew, Habits } from './pages'
 
 export class App extends Component {
     render() {
         return (
             <div>
-                
+                <h1>Habit Tracker!</h1>
+                <Switch>
+                    <Route path='/login' component={Login}/>
+                    <Route path='/addnew' component={AddNew}/> 
+                    <Route path='/habits' component={Habits}/> 
+                </Switch>
             </div>
         )
     }
 }
 
-export default App
+export default App;
