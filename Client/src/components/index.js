7e47d795a7266bc55a4b0fe3dde05089ae4b1f19
Anna Tran <annatran@annas-air.mynet>
2020-09-08 09:29:38 +0100

    add index.js

---
+++ Client/src/components/index.js
@@ -0,0 +1,9 @@
+export { default as AddNewCard } from './AddNewCard'
+export { default as HabitCard } from './HabitCard'
+export { default as Authentication } from './Authentication'
+export { default as LoginForm } from './LoginForm'
+export { default as Results } from './Results'
+export { default as LogOut } from './LogOut'
+export { default as Button } from './Button'
+
+
