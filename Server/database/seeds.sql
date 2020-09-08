3fb052ef64a492a056dcad9f6f7a644978d0540e
Anna Tran <annatran@annas-air.mynet>
2020-09-08 09:34:33 +0100

    add default value

--- Server/database/seeds.sql
+++ Server/database/seeds.sql
@@ -1,10 +1,17 @@
 DROP TABLE IF EXISTS habits;
+-- DROP TABLE IF EXISTS users;
 
 CREATE TABLE habits (
     id serial PRIMARY KEY,
     name varchar(50) NOT NULL,
-    count int DEFAULT 0
+    count INTEGER DEFAULT 0
 );
+
+-- CREATE TABLE users (
+--     id serial PRIMARY KEY,
+--     username varchar(50) NOT NULL,
+--     password varchar(100) NOT NULL
+-- );
 
 INSERT INTO habits (name)
 VALUES ('Exercise');
