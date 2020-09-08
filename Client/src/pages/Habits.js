6e9ee7b59e7249e13bb2e0170f03dbf296d9b7ce
Anna Tran <annatran@annas-air.mynet>
2020-09-08 17:36:55 +0100

    tidy code

--- Client/src/pages/Habits.js
+++ Client/src/pages/Habits.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import HabitCard from '../components/HabitCard'
 
+
 const Habits = () => {
     return (
-        <HabitCard/>
+        <>
+            <HabitCard/>
+        
+        </>
     )
 }
 
